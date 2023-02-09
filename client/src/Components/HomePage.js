ddc37578d9c3a6e028adb06e8a1563a6c7522cbc
Rohan Mote <irohanmote@gmail.com>
2023-02-09 18:11:26 +0530

    update

--- client/src/Components/HomePage.js
+++ client/src/Components/HomePage.js
@@ -18,12 +18,12 @@ const HomePage = () => {
     const [movieName, setMovieName] = useState(getLocalMovieName);
     const [timeSlot, setTimeSlot] = useState(getLocalTimeSlot);
     const [slots, setSlots] = useState({
-        A1: localStore.A1?localStore.A1:"",
-        A2: localStore.A2?localStore.A2:"",
-        A3: localStore.A3?localStore.A3:"",
-        A4: localStore.A4?localStore.A4:"",
-        D1: localStore.D1?localStore.D1:"",
-        D2: localStore.D2?localStore.D2:"",
+        A1: localStore?.A1 || "",
+        A2: localStore?.A2 || "",
+        A3: localStore?.A3 || "",
+        A4: localStore?.A4 || "",
+        D1: localStore?.D1 || "",
+        D2: localStore?.D2 || "",
     });
 
 
