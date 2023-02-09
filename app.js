16db9329c8922448f5fcaeae38b3f0aa0cb0c6c3
Rohan Mote <irohanmote@gmail.com>
2023-02-09 15:20:18 +0530

    Changes

--- app.js
+++ app.js
@@ -61,7 +61,7 @@ app.get("/api/booking", async (req, res) => {
 
 if (process.env.NODE_ENV == "production"){
 
-    app.use(express.static("client/build"));
+    app.use(express.static("/client/build"));
 
     // const path = require("path");
 
